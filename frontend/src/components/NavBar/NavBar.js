4be92907b3d874f9b35630efff2d4673d550ba82
j-koziel <jonathankoziel1@gmail.com>
2023-10-05 15:58:35 +0100

    feat: style the navbar

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,9 +1,14 @@
 import "./NavBar.css";
 import NavBarButton from "../NavBarButton/NavBarButton";
+import logo from "../../assets/logo.png";
 
 function NavBar() {
   return (
-    <div>
+    <div className="nav-bar">
+      <div className="title-container">
+        <img src={logo} alt="Titanic Fitness Logo" />
+        <h1>Titanic Fitness</h1>
+      </div>
       <div className="nav-bar-buttons-container">
         <NavBarButton buttonText="About" />
         <NavBarButton buttonText="Locations" />
