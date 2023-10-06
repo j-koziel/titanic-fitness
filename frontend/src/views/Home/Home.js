e51a1050ca0b576532072871da1c4637ea9c9728
j-koziel <jonathankoziel1@gmail.com>
2023-10-06 16:18:48 +0100

    feat(home page): create information boxes

--- frontend/src/views/Home/Home.js
+++ frontend/src/views/Home/Home.js
@@ -3,12 +3,30 @@ import homePageImg from "../../assets/home-page-img.jpg";
 
 function Home() {
   return (
-    <div>
+    <div className="home-page-container">
       <img
         className="home-page-img"
         src={homePageImg}
         alt="Man lifting barbell"
       />
+
+      <div className="information-boxes-container">
+        <div className="information-box">
+          <h2>Heading</h2>
+          <p>Text</p>
+          <button>Click me</button>
+        </div>
+        <div className="information-box">
+          <h2>Heading</h2>
+          <p>Text</p>
+          <button>Click me</button>
+        </div>
+        <div className="information-box">
+          <h2>Heading</h2>
+          <p>Text</p>
+          <button>Click me</button>
+        </div>
+      </div>
     </div>
   );
 }
