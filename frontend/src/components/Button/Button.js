09d4f1ba3762a19863e170ddbd9c839767d1cb0b
j-koziel <jonathankoziel1@gmail.com>
2023-10-10 11:41:40 +0100

    feat(home page): add animations and text to the home page

---
+++ frontend/src/components/Button/Button.js
@@ -0,0 +1,7 @@
+import "./Button.css";
+
+function Button({ text }) {
+  return <button className="button-component">{text}</button>;
+}
+
+export default Button;
--- frontend/src/views/Home/Home.js
+++ frontend/src/views/Home/Home.js
@@ -1,5 +1,6 @@
 import "./Home.css";
 import homePageImg from "../../assets/home-page-img.jpg";
+import Button from "../../components/Button/Button";
 
 function Home() {
   return (
@@ -12,19 +13,27 @@ function Home() {
 
       <div className="information-boxes-container">
         <div className="information-box">
-          <h2>Heading</h2>
-          <p>Text</p>
-          <button>Click me</button>
+          <h2>Our Facilities</h2>
+          <p>
+            We have 4 great facilities located across West London. Come visit us
+            today!
+          </p>
+          <Button text="Find A Gym" />
         </div>
         <div className="information-box">
-          <h2>Heading</h2>
-          <p>Text</p>
-          <button>Click me</button>
+          <h2>Our Plans</h2>
+          <p>
+            Our subscription service will provide you with <em>all</em> that you
+            need to <strong>attain</strong> your fitness goals.
+          </p>
+          <Button text="Register" />
         </div>
         <div className="information-box">
-          <h2>Heading</h2>
-          <p>Text</p>
-          <button>Click me</button>
+          <h2>Log In</h2>
+          <p>
+            Log in to your account to get back on track to reaching your goals
+          </p>
+          <Button text="Log In" />
         </div>
       </div>
     </div>
