be0f9b069d30cc06c4416341d11b1231053cfdaf
j-koziel <jonathankoziel1@gmail.com>
2023-10-17 12:46:45 +0100

    feat: create the settings page

--- frontend/src/views/Settings/Settings.js
+++ frontend/src/views/Settings/Settings.js
@@ -1,5 +1,6 @@
 import "./Settings.css";
 import NavBarButton from "../../components/NavBarButton/NavBarButton";
+import ProfilePic from "../../assets/avatar.jpg";
 
 function Settings() {
   return (
@@ -14,33 +15,45 @@ function Settings() {
           </div>
         </div>
         <div className="settings-main">
-          <h1>Your Account</h1>
-          <form>
-            <div className="settings-input">
-              <label htmlFor="full-name-input">Full Name</label>
-              <input type="text" placeholder="Full Name" id="full-name-input" />
-            </div>
-            <div className="settings-input">
-              <label htmlFor="date-of-birth-input">Date of Birth</label>
-              <input
-                type="text"
-                placeholder="Date Of Birth"
-                id="date-of-birth-input"
-              />
-            </div>
-            <div className="settings-input">
-              <label htmlFor="email-input">Change Email</label>
-              <input type="email" placeholder="Email" id="email-input" />
-            </div>
-            <div className="settings-input">
-              <label htmlFor="password-input">Reset Password</label>
-              <input
-                type="password"
-                placeholder="Password"
-                id="password-input"
-              />
-            </div>
-          </form>
+          <div className="account-details-container">
+            <h1>Your Account</h1>
+            <form className="account-details-form">
+              <div className="settings-input">
+                <label htmlFor="full-name-input">Full Name</label>
+                <input
+                  type="text"
+                  placeholder="Full Name"
+                  id="full-name-input"
+                />
+              </div>
+              <div className="settings-input">
+                <label htmlFor="date-of-birth-input">Date of Birth</label>
+                <input
+                  type="text"
+                  placeholder="Date Of Birth"
+                  id="date-of-birth-input"
+                />
+              </div>
+              <div className="settings-input">
+                <label htmlFor="email-input">Change Email</label>
+                <input type="email" placeholder="Email" id="email-input" />
+              </div>
+              <div className="settings-input">
+                <label htmlFor="password-input">Reset Password</label>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  id="password-input"
+                />
+              </div>
+              <button type="submit">Save Changes</button>
+            </form>
+          </div>
+          <div className="profile-pic-container">
+            <img src={ProfilePic} alt="You" />
+            <button className="change-avatar-btn">Change Avatar</button>
+            <button className="del-avatar-btn">Delete Avatar</button>
+          </div>
         </div>
       </div>
     </div>
