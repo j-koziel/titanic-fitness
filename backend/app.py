54fcc008258319cb0556402722fd1be7bf33cede
j-koziel <jonathankoziel1@gmail.com>
2023-10-31 10:58:36 +0000

    feat: create user endpoint

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,14 @@ app = FastAPI()
 
 db = load_db("./db.json")
 
+@app.get("/")
+def home():
+  return "Welcome to the Titanic Fitness API"
+
 @app.get("/api/v1/users")
 def get_all_users():
   return db
+
+@app.post("/api/v1/users")
+def create_new_user():
+  return "new user created"
